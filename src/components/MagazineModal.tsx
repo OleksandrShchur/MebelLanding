6352aba54ef5a8f82c7dbca4c7b4f190799e3f9a
Oleksandr Shchur <osur12709@gmail.com>
2026-03-12 12:02:46 +0200

    hide catalog details on modal

--- src/components/MagazineModal.tsx
+++ src/components/MagazineModal.tsx
@@ -43,10 +43,10 @@ const MagazineModal = ({ magazine, isOpen, onClose }: MagazineModalProps) => {
         </button>
 
         {/* Magazine Info */}
-        <div className="absolute top-4 left-4 z-10 bg-white bg-opacity-90 p-4 rounded-lg border border-[#E6E1DA]">
+        {/* <div className="absolute top-4 left-4 z-10 bg-white bg-opacity-90 p-4 rounded-lg border border-[#E6E1DA]">
           <h2 className="text-xl font-bold text-[#2F2A25]">{magazine.name}</h2>
           <p className="text-[#5B544E]">{magazine.description}</p>
-        </div>
+        </div> */}
 
         {/* Flip Book Viewer */}
         <FlipBookViewer images={magazine.images} orientation={magazine.orientation} pageDimensions={magazine.page} />
