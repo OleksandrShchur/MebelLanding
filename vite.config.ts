31b2f2b114e73d4b2ebc7fd281c92ee394916939
Oleksandr Shchur <osur12709@gmail.com>
2026-06-30 22:29:44 +0300

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { catalogImagesPlugin } from './vite-plugins/catalog-images'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), catalogImagesPlugin()],
-  base: '/MebelLanding/',
+  plugins: [react()],
+  base: '/',
 })
